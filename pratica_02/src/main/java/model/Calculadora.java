c61431f0cec789f53bd6cf249c917a0f897beb43
Thiago Dias <thiagodiaspenha@gmail.com>
2021-06-25 16:29:12 -0300

    Prática 02 - Fase Azul

--- pratica_02/src/main/java/model/Calculadora.java
+++ pratica_02/src/main/java/model/Calculadora.java
@@ -3,27 +3,40 @@ package model;
 public class Calculadora {
 
     //Este metodo ira realizar o calculo do arranjo.
-    public int arranjo (int n, int p) {
-        if(n > 0 && p >0){
-        int fatN = 1;
-        for (int i = 1; i <= n; i = i + 1) {
-            fatN = fatN * i;
-        }
-        int fatP = 1;
-        for (int i = 1; i <= p; i = i + 1) {
-            fatP = fatP * i;
-        }
-        int fatNP = 1;
-        for (int i = 1; i <= (n-p); i = i + 1) {
-            fatNP = fatNP * i;
-        }
-        int arranjo = fatN/fatNP;
-        System.out.println(fatN);
-        System.out.println(fatNP);
-        return arranjo;
+    public static int fatorialN(int n) {
+        int resultadoN = 1;
+        if (n > 0){
+            while( n > 0) {
+                resultadoN = resultadoN * n;
+                n--;
+            }
+            return resultadoN;
         }else{
             return 0;
         }
     }
 
+    public static int fatorialNP(int n ,int p) {
+        int resultadoNP = 1;
+        int np = (n-p);
+        if(n > 0 && p > 0){
+            while( np > 0) {
+                resultadoNP = resultadoNP * np;
+                np--;
+            }
+            return resultadoNP;
+        }else{
+            return 0;
+        }
+    }
+
+
+    public static int arranjo (int n, int p ) {
+        if(n > 0 && p > 0){
+            return fatorialN(n)/fatorialNP(n,p);
+        }else{
+            return 0;
+        }
+    }
 }
+
