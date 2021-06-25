8987b9c5068668c0fd885676db8d8328e9232225
Thiago Dias <thiagodiaspenha@gmail.com>
2021-06-25 16:23:52 -0300

    Prática 02 - Fase Verde

--- pratica_02/src/main/java/model/Calculadora.java
+++ pratica_02/src/main/java/model/Calculadora.java
@@ -4,7 +4,26 @@ public class Calculadora {
 
     //Este metodo ira realizar o calculo do arranjo.
     public int arranjo (int n, int p) {
-        return 55;
+        if(n > 0 && p >0){
+        int fatN = 1;
+        for (int i = 1; i <= n; i = i + 1) {
+            fatN = fatN * i;
+        }
+        int fatP = 1;
+        for (int i = 1; i <= p; i = i + 1) {
+            fatP = fatP * i;
+        }
+        int fatNP = 1;
+        for (int i = 1; i <= (n-p); i = i + 1) {
+            fatNP = fatNP * i;
+        }
+        int arranjo = fatN/fatNP;
+        System.out.println(fatN);
+        System.out.println(fatNP);
+        return arranjo;
+        }else{
+            return 0;
+        }
     }
 
 }
