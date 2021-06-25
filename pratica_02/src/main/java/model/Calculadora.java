ad62f0cbad96d32dec9cd8eb1c9527b03068e7db
Thiago Dias <thiagodiaspenha@gmail.com>
2021-06-25 16:18:44 -0300

    Prática 02 - Fase Vermelha

---
+++ pratica_02/src/main/java/model/Calculadora.java
@@ -0,0 +1,10 @@
+package model;
+
+public class Calculadora {
+
+    //Este metodo ira realizar o calculo do arranjo.
+    public int arranjo (int n, int p) {
+        return 55;
+    }
+
+}
---
+++ pratica_02/src/test/java/model/CalculadoraTest.java
@@ -0,0 +1,38 @@
+package model;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculadoraTest {
+
+    Calculadora calc = new Calculadora();
+
+    //PARTICAO 1
+    //VALOR MAIS PERTO DE 0
+    //CT: <1,1>
+    @Test
+    public void testeArranjoPertoZero() {
+        int resultado = calc.arranjo(1,1);
+        assertEquals(1,resultado);
+    }
+
+    //VALOR MAIS PERTO DE 5
+    //CT: <5,120>
+    @Test
+    public void testeArranjoPertoCinco() {
+        int resultado = calc.arranjo(5,5);
+        assertEquals(120,resultado);
+    }
+
+    //PARTICAO NULA
+    // CT:<0,0>
+    @Test
+    public void testeArranjoZero() {
+        double resultado = calc.arranjo(0,0);
+        assertEquals(0, resultado);
+    }
+}
